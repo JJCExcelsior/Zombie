02a034b8b8e9500a9e4665559151008b46f0d749
JJCExcelsior <71657149+JJCExcelsior@users.noreply.github.com>
2020-10-01 14:45:38 +0200

    Lives

--- script.js
+++ script.js
@@ -1,128 +1,156 @@
-let canvas = document.querySelector('canvas')
-canvas.style.border = '1px solid black'
-
-let ctx = canvas.getContext('2d')
-let intervalId = 0
-
-let bg = new Image()
-bg.src = 'images/bgnew.png'
-
-let knight = new Image()
-knight.src = 'images/kn.png'
-
-let zombie = new Image()
-zombie.src = 'images/nz.png'
-
-let ground = new Image()
-ground.src = 'images/ground.png'
+let canvas = document.querySelector("canvas");
+canvas.style.border = "1px solid black";
+let ctx = canvas.getContext("2d");
+let intervalId = 0;
+let bg = new Image();
+bg.src = "images/bgnew.png";
+let knight = new Image();
+knight.src = "images/kn.png";
+let zombie = new Image();
+zombie.src = "images/nz.png";
+let ground = new Image();
+ground.src = "images/ground.png";
 
 let knightX = 240;
 let knightY = 490;
-let knightkeyboard = 10
-let knightIncrement = 1
-let knightJump = 120
+let knightkeyboard = 10;
+let knightIncrement = 1;
+let knightJump = 120;
 
 let zombiesX = 800;
 let zombiesY = 490;
 
 let zombieSpeed = 10;
 
-let zombiess = [
-    {x: canvas.width-20, y: 490}
-]
+let zombiess = [{ x: canvas.width - 20, y: 490 }];
 
-let score = 0
+let score = 0;
+let lives = 0;
 
-window.onload= function(){
-  document.getElementById("play").play()
-}
+window.onload = function () {
+  document.getElementById("play").play();
+};
 
 document.onkeydown = (event) => {
-    console.log(event.keyCode);
-    if (event.keyCode == 39 || event.key == "ArrowRight") {
-      knightX += knightkeyboard
+  console.log(event.keyCode);
+  if (event.keyCode == 39 || event.key == "ArrowRight") {
+    knightX += knightkeyboard;
 
-      isLeftArrow = false;
-    } if (event.keyCode == 37 || event.key == "ArrowLeft") {
-      knightX -= knightkeyboard
-    }  if (event.keyCode == 32 || event.key == "Space") {
-     if (knightY> (canvas.height-120)-120)
-      knightY -= knightJump;
-  };
-}
+    isLeftArrow = false;
+  }
+  if (event.keyCode == 37 || event.key == "ArrowLeft") {
+    knightX -= knightkeyboard;
+  }
+  if (event.keyCode == 32 || event.key == "Space") {
+    if (knightY > canvas.height - 120 - 120) knightY -= knightJump;
+  }
+};
 
 document.onkeyup = (event) => {
   if (event.keyCode == 32 || event.key == "Space") {
-    knightY += knightJump
+    knightY += knightJump;
+  }
 };
 
-}
-
-function knightCanvasCollision(){
-  if (knightX> canvas.width-50){
-    knightX = canvas.width-50;
-  } else if (knightX-10 <=0){
+function knightCanvasCollision() {
+  if (knightX > canvas.width - 50) {
+    knightX = canvas.width - 50;
+  } else if (knightX - 10 <= 0) {
     knightX = 10;
   }
 }
-function drawZombies(){
-  for (let i=0; i< zombiess.length; i++){
-    ctx.drawImage(zombie, zombiess[i].x, zombiess[i].y)
-   
-    zombiess[i].x-=zombieSpeed
+
+function drawZombies() {
+  for (let i = 0; i < zombiess.length; i++) {
+    ctx.drawImage(zombie, zombiess[i].x, zombiess[i].y);
+
+    zombiess[i].x -= zombieSpeed;
 
     if (zombiess)
+      if (zombiess[i].x == 20) {
+        score += 10;
+        zombiess.push({
+          x: zombiesX,
+          y: zombiesY,
+        });
+      }
+  }
+}
 
-    if (zombiess[i].x == 20) {
-      score += 10
-      zombiess.push({
-            x: zombiesX,
-            y: zombiesY,
-        })
-    }
+function win (){
+  if (score == 20){
+    console.log(score)
+    clearInterval(intervalId);
+    location.href = "win.html"
   }
 }
 
 function drawScore() {
-    ctx.font = "bold 16px Arial";
-    ctx.fillStyle = "#f9f9f9";
-    ctx.fillText("Score: "+score, 10, 20);
+  ctx.font = "bold 16px Arial";
+  ctx.fillStyle = "#f9f9f9";
+  ctx.fillText("Score: " + score, 10, 20);
 }
 
- 
+function drawLives(lives) {
+  ctx.font = "bold 16px Arial";
+  ctx.fillStyle = "#FF0000";
+  ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
+}
 
-function collision (){
-  for (let i=0; i< zombiess.length; i++){
-    if (knightX < zombiess[i].x + zombie.width && knightX + knight.width > zombiess[i].x && knightY + knight.height > zombiess[i].y)
-    {
-      location.href = 'gameover.html'
+function collision() {
+  for (let i = 0; i < zombiess.length; i++) {
+    if (
+      knightX < zombiess[i].x + zombie.width &&
+      knightX + knight.width > zombiess[i].x &&
+      knightY + knight.height > zombiess[i].y
+    ) {
+      lives--;
+      if (lives === 0) {
+        clearInterval(intervalId);
+        // draw the lives wth the 0
+        drawLives(lives);
+        setTimeout(() => {
+          location.href = "gameover.html";
+        }, 500);
+      } else {
+        resetPositions();
+        // clear the set interbval
+        clearInterval(intervalId);
+
+        intervalId = setInterval(() => {
+          requestAnimationFrame(() => restartGame(lives));
+        }, 50);
+      }
     }
-    // if (knightX === zombiess[i].x && knightY === zombiess[i].y){
-    //   // alert ('Zombies moving knight still')
-    //   location.href = 'gameover.html'
-    // } 
-    // else if (knightX - knight.width === zombiess[i].x + zombie.width){
-    //   alert ('Zombies on the left of the knight')
-    // }
   }
 }
 
-function audio (){
-  let intro = new Audio('./sounds/game.mp3')
-  intro.play()
+function resetPositions() {
+  knightX = 240;
+  knightY = 490;
+  zombiess = [{ x: canvas.width - 20, y: 490 }];
 }
 
-function startGame (){
-    // audio()
-    ctx.drawImage(bg, 0, 0)
-    ctx.drawImage(knight, knightX, knightY)
-    ctx.drawImage(ground, 0, canvas.height-120)
-    knightCanvasCollision()
-    drawZombies()
-    collision()
-    drawScore()
+function restartGame(life) {
+  console.log(life);
+  lives = life;
+  console.log(lives);
+  ctx.drawImage(bg, 0, 0);
+  ctx.drawImage(knight, knightX, knightY);
+  ctx.drawImage(ground, 0, canvas.height - 120);
+  knightCanvasCollision();
+  drawZombies();
+  drawScore();
+  drawLives(lives);
+  collision();
+  win()
 }
 
-intervalId = setInterval(()=>{
-    requestAnimationFrame(startGame)
-}, 50)
+function startGame() {
+  // audio()
+  restartGame(3);
+}
+
+intervalId = setInterval(() => {
+  requestAnimationFrame(startGame);
+}, 50);
