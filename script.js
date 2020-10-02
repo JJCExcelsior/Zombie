7653da80d118efa370f3cd43a459dda0fddec883
JJCExcelsior <71657149+JJCExcelsior@users.noreply.github.com>
2020-10-02 14:46:34 +0200

    Fixed bugs

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let canvas = document.querySelector("myCanvas");
+let canvas = document.querySelector("canvas");
 canvas.style.border = "1px solid black";
 let ctx = canvas.getContext("2d");
 let intervalId = 0;
@@ -8,55 +8,55 @@ let intervalId = 0;
 
 // startGame
 
-let body = document.querySelector("body")
-let splash = document.querySelector("#splash-container")
-let button = document.querySelector("#startBtn")
+// let body = document.querySelector("body")
+// let splash = document.querySelector("#splash-container")
+// let button = document.querySelector("#startBtn")
 
 
 
-button.addEventListener('click', () => {
-  //when clicked removes the splash screen 
-  splash.style.display = "none" //remove splash screen
-  //add the canvas
-  canvas.style.display = "flex"
+// button.addEventListener('click', () => {
+//   //when clicked removes the splash screen 
+//   splash.style.display = "none" //remove splash screen
+//   //add the canvas
+//   canvas.style.display = "flex"
 
-  intervalId = setInterval (() => {
-    requestAnimationFrame(startGame)
-  })
-})
+//   intervalId = setInterval (() => {
+//     requestAnimationFrame(startGame)
+//   })
+// })
 
-// Game Over
+// // Game Over
 
-let gameOver = document.querySelector(".gameover-container")
+// let gameOver = document.querySelector(".gameover-container")
 
 
-button.addEventListener('click', () => {
-  //when clicked removes the splash screen 
-  gameover-container.parentNode.removeChild(gameover) //remove splash screen
-  //add the canvas
-  body.appendChild("splash-container")
+// button.addEventListener('click', () => {
+//   //when clicked removes the splash screen 
+//   gameover-container.parentNode.removeChild(gameover) //remove splash screen
+//   //add the canvas
+//   body.appendChild("splash-container")
 
-})
+// })
 
 // Win
 
-let win = document.querySelector("win-container")
+// let win = document.querySelector("win-container")
 
-button.addEventListener('click', () => {
-  //when clicked removes the splash screen 
-  win-container.parentNode.removeChild(splash) //remove splash screen
-  //add the canvas
-  body.appendChild(canvasContainer)
+// button.addEventListener('click', () => {
+//   //when clicked removes the splash screen 
+//   win-container.parentNode.removeChild(splash) //remove splash screen
+//   //add the canvas
+//   body.appendChild(canvasContainer)
 
-  canvas = document.querySelector('canvas')
+//   canvas = document.querySelector('canvas')
 
-  ctx = canvas.getContext('2d')
-  intervalId = 0;
+//   ctx = canvas.getContext('2d')
+//   intervalId = 0;
 
-  intervalId = setInterval (() => {
-    requestAnimationFrame(restartGame)
-  })
-})
+//   intervalId = setInterval (() => {
+//     requestAnimationFrame(restartGame)
+//   })
+// })
 
 let bg = new Image();
 bg.src = "images/bgnew.png";
@@ -101,6 +101,7 @@ document.onkeydown = (event) => {
   }
   if (event.keyCode == 32 || event.key == "Space") {
     if (knightY > canvas.height - 120 - 120) knightY -= knightJump;
+    
   }
 };
 
@@ -110,104 +111,104 @@ document.onkeyup = (event) => {
   }
 };
 
-// function knightCanvasCollision() {
-//   if (knightX > canvas.width - 50) {
-//     knightX = canvas.width - 50;
-//   } else if (knightX - 10 <= 0) {
-//     knightX = 10;
-//   }
-// }
+function knightCanvasCollision() {
+  if (knightX > canvas.width - 50) {
+    knightX = canvas.width - 50;
+  } else if (knightX - 10 <= 0) {
+    knightX = 10;
+  }
+}
 
-// function drawZombies() {
-//   for (let i = 0; i < zombiess.length; i++) {
-//     ctx.drawImage(zombie, zombiess[i].x, zombiess[i].y);
+function drawZombies() {
+  for (let i = 0; i < zombiess.length; i++) {
+    ctx.drawImage(zombie, zombiess[i].x, zombiess[i].y);
 
-//     zombiess[i].x -= zombieSpeed;
+    zombiess[i].x -= zombieSpeed;
 
-//     if (zombiess)
-//       if (zombiess[i].x == 20) {
-//         score += 10;
-//         zombiess.push({
-//           x: zombiesX,
-//           y: zombiesY,
-//         });
-//       }
-//   }
-// }
+    if (zombiess)
+      if (zombiess[i].x == 20) {
+        score += 10;
+        zombiess.push({
+          x: zombiesX,
+          y: zombiesY,
+        });
+      }
+  }
+}
 
-// function win(){
-//   if (score == 20){
-//     console.log(score)
-//     clearInterval(intervalId);
-//     location.href = "win.html"
-//   }
-// }
+function winCondition(){
+  if (score == 200){
+    console.log(score)
+    clearInterval(intervalId);
+    location.href = "win.html"
+  }
+}
 
-// function drawScore() {
-//   ctx.font = "bold 16px Arial";
-//   ctx.fillStyle = "#f9f9f9";
-//   ctx.fillText("Score: " + score, 10, 20);
-// }
+function drawScore() {
+  ctx.font = "bold 16px Arial";
+  ctx.fillStyle = "#f9f9f9";
+  ctx.fillText("Score: " + score, 10, 20);
+}
 
-// function drawLives(lives) {
-//   ctx.font = "bold 16px Arial";
-//   ctx.fillStyle = "#FF0000";
-//   ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
-// }
+function drawLives(lives) {
+  ctx.font = "bold 16px Arial";
+  ctx.fillStyle = "#FF0000";
+  ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
+}
 
-// function collision() {
-//   for (let i = 0; i < zombiess.length; i++) {
-//     if (
-//       knightX < zombiess[i].x + zombie.width &&
-//       knightX + knight.width > zombiess[i].x &&
-//       knightY + knight.height > zombiess[i].y
-//     ) {
-//       lives--;
-//       if (lives === 0) {
-//         clearInterval(intervalId);
-//         // draw the lives wth the 0
-//         drawLives(lives);
-//         setTimeout(() => {
-//           location.href = "gameover.html";
-//         }, 500);
-//       } else {
-//         resetPositions();
-//         // clear the set interbval
-//         clearInterval(intervalId);
+function collision() {
+  for (let i = 0; i < zombiess.length; i++) {
+    if (
+      knightX < zombiess[i].x + zombie.width &&
+      knightX + knight.width > zombiess[i].x &&
+      knightY + knight.height > zombiess[i].y
+    ) {
+      lives--;
+      if (lives === 0) {
+        clearInterval(intervalId);
+        // draw the lives wth the 0
+        drawLives(lives);
+        setTimeout(() => {
+          location.href = "gameover.html";
+        }, 500);
+      } else {
+        resetPositions();
+        // clear the set interbval
+        clearInterval(intervalId);
 
-//         intervalId = setInterval(() => {
-//           requestAnimationFrame(() => restartGame(lives));
-//         }, 50);
-//       }
-//     }
-//   }
-// }
+        intervalId = setInterval(() => {
+          requestAnimationFrame(() => restartGame(lives));
+        }, 50);
+      }
+    }
+  }
+}
 
-// function resetPositions() {
-//   knightX = 240;
-//   knightY = 490;
-//   zombiess = [{ x: canvas.width - 20, y: 490 }];
-// }
+function resetPositions() {
+  knightX = 240;
+  knightY = 490;
+  zombiess = [{ x: canvas.width - 20, y: 490 }];
+}
 
-// function restartGame(life) {
-//   console.log(life);
-//   lives = life;
-//   console.log(lives);
-//   ctx.drawImage(bg, 0, 0);
-//   ctx.drawImage(knight, knightX, knightY);
-//   ctx.drawImage(ground, 0, canvas.height - 120);
-//   knightCanvasCollision();
-//   drawZombies();
-//   drawScore();
-//   drawLives(lives);
-//   collision();
-//   win();
-// }
+function restartGame(life) {
+  console.log(life);
+  lives = life;
+  console.log(lives);
+  ctx.drawImage(bg, 0, 0);
+  ctx.drawImage(knight, knightX, knightY);
+  ctx.drawImage(ground, 0, canvas.height - 120);
+  knightCanvasCollision();
+  drawZombies();
+  drawScore();
+  drawLives(lives);
+  collision();
+  winCondition();
+}
 
-// function startGame() {
-//   // audio()
-//   restartGame(3);
-// }
+function startGame() {
+  // audio()
+  restartGame(3);
+}
 
 intervalId = setInterval(() => {
   requestAnimationFrame(startGame);
